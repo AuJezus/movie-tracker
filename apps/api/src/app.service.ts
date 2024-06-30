6beca0913785a80109b78671f3c69d91ba73209b
aujezus <augispay@gmail.com>
2024-06-30 11:44:46 +0300

    Add database package to nestjs api

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import { db } from "database";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  async getHello() {
+    const res = await db.query.posts.findFirst({ columns: { title: true } });
+
+    console.log(res.title);
     return "Hello World!";
   }
 }
